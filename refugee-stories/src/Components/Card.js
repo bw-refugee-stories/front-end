582160771649a50ab0d5154a5c80c7b4a3550411
josepaniagua <jpanjr@gmail.com>
2019-09-21 12:22:43 -0700

    created pend component for card component

--- refugee-stories/src/Components/Card.js
+++ refugee-stories/src/Components/Card.js
@@ -1,3 +1,22 @@
 import React from "react";
+import { Card } from "semantic-ui-react";
+import Pend from "../Components/Pend.js";
 
-const Card = () =>
+
+const pend =(
+    <p>
+        <Icon name="circle outline" />
+        Pending
+    </p>
+);
+
+const CardForStories = (props) => (
+    <Card
+        header={props.title}
+        meta={props.name} // remember to add props.email
+        description={props.contents}
+        extra={Pend}
+    />
+);
+
+export default CardForStories;
