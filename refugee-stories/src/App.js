b1d3943b03d153301c5ac7bdd08f6468969af78b
josepaniagua <jpanjr@gmail.com>
2019-09-21 21:58:25 -0700

    accessed refugee api

--- refugee-stories/src/App.js
+++ refugee-stories/src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from "axios";
+import CardList from "./Components/CardList";
 
-function App() {
+const App = () => {
+  const [story, setStory] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://refu-stories-api.herokuapp.com/stories')
+        .then(res => {
+          console.log(res.data);
+        })
+        .catch(err => console.log('Error: ', err))
+  }, []);
+
   return (
-    <div className="App">
-      hello
-    </div>
+      <div className="App">
+        hello
+      </div>
   );
-}
+};
 
 export default App;
---
+++ refugee-stories/src/Components/CardList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const CardList = (props) => (
+    <div>
+        {}
+    </div>
+);
+
+export default CardList;
