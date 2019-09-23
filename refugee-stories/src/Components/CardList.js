2a9827d69aed7f1c7957d121fe1f8087f3181186
josepaniagua <jpanjr@gmail.com>
2019-09-22 22:42:11 -0700

    styled cards and background

--- refugee-stories/src/Components/CardList.js
+++ refugee-stories/src/Components/CardList.js
@@ -1,10 +1,12 @@
 import React from "react";
 import CardForStories from "./Card";
+import "../App.css";
 
 const CardList = (props) => (
-    <div>
+    <div className="card-cont">
         {props.cuento.map(item => {
             return <CardForStories
+                        className="card"
                         key={item.id}
                         title={item.title}
                         name={`Author: ${(item.name !== null) ? item.name : "N/A"}  -  Email: ${(item.email !== null) ? item.email : "N/A"}`}
