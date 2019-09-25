3a855143a611151c1de3d42e7edf139638b93498
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2019-09-25 12:30:35 -0400

    Added map.geojason and hardcoded some Markers

--- refugee-stories/src/components/Connect/Connect.jsx
+++ refugee-stories/src/components/Connect/Connect.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { GoogleApiWrapper, InfoWindow, Marker, Map } from 'google-maps-react';
 import { api_key } from '../../data/data';
 import CurrentLocation from './Map';
@@ -21,6 +21,9 @@ const MarkersList = props => {
   );
 };
 
+// function MapContainer({showingInfoWindow, activeMarker, selectedPlace, locations}) {
+//   const [state, setState]
+// }
 
 export class MapContainer extends Component {
   constructor(props) {
@@ -29,7 +32,8 @@ export class MapContainer extends Component {
       showingInfoWindow: false,
       activeMarker: {},
       selectedPlace: {},
-      locations: []
+      locations: [],
+      showingName: false
     };
     // this.handleMapClick = this.handleMapClick.bind(this);
   }
@@ -49,12 +53,14 @@ export class MapContainer extends Component {
   // };
 
 
-  onMarkerClick = (props, marker, e) =>
+  onMarkerClick = (props, marker, name, e) =>
     this.setState({
       selectedPlace: props,
       activeMarker: marker,
-      showingInfoWindow: true
-    });
+      showingInfoWindow: true,
+      showingName: true
+    },
+    );
 
   onClose = props => {
     if (this.state.showingInfoWindow) {
@@ -69,6 +75,10 @@ export class MapContainer extends Component {
     return (
       <CurrentLocation centerAroundCurrentLocation google={this.props.google}>
         <Marker onClick={this.onMarkerClick} name={'current location'} name/>
+        <Marker name={'Human Rights First'}
+            position={{lat: 40.70462347898018, lng: -74.0114164352417}} />
+        <Marker name={'Human Rights Watch'}
+            position={{lat: 40.74841927463572, lng: -73.98565918207169}} />
           <InfoWindow marker={this.state.activeMarker} visible={this.state.showingInfoWindow} onClose={this.onClose}>
             <div>
               <h4>{this.state.selectedPlace.name}</h4>
